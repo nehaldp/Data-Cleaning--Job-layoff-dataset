3a98b3dc098f6d3eda182b0ca45fa76962fc34ef
Neha L <157330113+nehaldp@users.noreply.github.com>
2024-10-28 10:56:12 -0500

    Add files via upload

---
+++ Data_Cleaning_Job_Layoff_Trends.sql
@@ -0,0 +1,127 @@
+-- Data Cleaning using SQL
+-- https://www.kaggle.com/datasets/swaptr/layoffs-2022?resource=download
+SELECT *
+FROM world_layoffs_latest.layoffs;
+
+-- Creating copy of RAW data to work on
+CREATE TABLE world_layoffs_latest.layoffs_staging
+LIKE world_layoffs_latest.layoffs;
+
+INSERT world_layoffs_latest.layoffs_staging
+SELECT *
+FROM world_layoffs_latest.layoffs;
+
+SELECT *
+FROM world_layoffs_latest.layoffs_staging;
+
+-- 1. Remove Duplicates.
+-- 2. Standardize the Data.
+-- 3. Null Values and Blank Values.
+-- 4. Remove any Columns that aren't needed.
+
+-- 1. Remove Duplicates.
+-- Since there are no ID column, include row_number to discover duplicates. 
+SELECT company, location, industry, total_laid_off, percentage_laid_off, 
+`date`, stage, country, funds_raised,
+ROW_NUMBER() OVER(PARTITION BY company, industry, total_laid_off,`date`) as row_num
+FROM world_layoffs_latest.layoffs_staging;
+
+-- Identifying duplicates (row_num > 1)
+WITH Duplicate_CTEs AS
+(
+SELECT company, location, industry, total_laid_off, percentage_laid_off, 
+`date`, stage, country, funds_raised,
+ROW_NUMBER() OVER(PARTITION BY company, industry, total_laid_off,`date`) as row_num
+FROM world_layoffs_latest.layoffs_staging
+)
+SELECT *
+FROM Duplicate_CTEs
+WHERE row_num > 1; -- Cannot make update/delete function on rows in existing table with CTE. 
+				   -- So create another table and delete duplicates
+
+CREATE TABLE `world_layoffs_latest`.`layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` text,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised` text,
+  `row_num` INT
+); 
+
+SELECT * 
+FROM world_layoffs_latest.layoffs_staging2;
+
+INSERT INTO world_layoffs_latest.layoffs_staging2
+SELECT *,
+ROW_NUMBER() OVER(PARTITION BY company, industry, total_laid_off,`date`) as row_num
+FROM world_layoffs_latest.layoffs_staging;
+
+-- Deleting the duplicate entries
+DELETE
+FROM world_layoffs_latest.layoffs_staging2
+WHERE row_num > 1;
+
+SELECT *
+FROM world_layoffs_latest.layoffs_staging2
+WHERE row_num > 1;
+
+-- 2. Standardize the Data.
+SELECT distinct company, trim(company)
+FROM world_layoffs_latest.layoffs_staging2;
+
+UPDATE world_layoffs_latest.layoffs_staging2 -- Removing spaces before and after company name 
+SET company = TRIM(company);
+
+SELECT distinct industry #Looks fine
+FROM world_layoffs_latest.layoffs_staging2
+ORDER BY 1;
+
+SELECT distinct location #Looks fine
+FROM world_layoffs_latest.layoffs_staging2;
+
+SELECT distinct country #Looks fine
+FROM world_layoffs_latest.layoffs_staging2
+order by 1;
+
+-- Data type of 'date' column is in text. Changing it to Date datatype.
+SELECT `date`
+FROM world_layoffs_latest.layoffs_staging2;
+
+ALTER TABLE world_layoffs_latest.layoffs_staging2
+MODIFY COLUMN `date` DATE;
+
+-- 3. Null Values and Blank Values.
+SELECT *
+FROM world_layoffs_latest.layoffs_staging2
+WHERE industry IS NULL or industry = '';  #company 'Appsmith' has blank industry
+
+SELECT *
+FROM world_layoffs_latest.layoffs_staging2
+WHERE company = 'Appsmith'; #No other entries for company so cannot modify
+
+/* Deleting rows where total_laid_off and percentage_laid_off are both null as 
+we cannot work with them - Useless data*/
+SELECT *
+FROM world_layoffs_latest.layoffs_staging2
+WHERE (total_laid_off IS NULL or total_laid_off = '')
+AND (percentage_laid_off IS NULL or percentage_laid_off = '');
+
+DELETE
+FROM world_layoffs_latest.layoffs_staging2
+WHERE (total_laid_off IS NULL or total_laid_off = '')
+AND (percentage_laid_off IS NULL or percentage_laid_off = '');
+
+SELECT *
+FROM world_layoffs_latest.layoffs_staging2;
+
+-- 4. Remove any Columns that aren't needed.
+-- Getting rid of the 'row_num' column
+ALTER TABLE world_layoffs_latest.layoffs_staging2
+DROP COLUMN row_num;
+
+SELECT *
+FROM world_layoffs_latest.layoffs_staging2;
